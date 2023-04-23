26a997e7e1793775a0f8231d7334eceae6e01cae
Aditya <adityavishal903@gmail.com>
2023-04-23 10:06:19 +0530

    1

--- Hashing/CountChar.cpp
+++ Hashing/CountChar.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 int main(){
     string s;
+    cout<<"Enter the string : ";
     cin >> s;
 
 
     //pre compute
     int hash[26]={0};
     for(int i=0; i<s.size(); i++){
-        hash[s[i]-'a']
+        hash[s[i]-'a']++;
     }
 
     int q;
